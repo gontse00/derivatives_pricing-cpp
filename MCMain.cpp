4a9e71dd03dd2228b62dd6ff9cc8d43148a4adc1
Gontse Kekane <kekanegontse@gmail.com>
2019-11-15 00:26:08 +0200

    chapter 3 inheritence and virtual functions: 3.5 not knowing the type & virtual destruction

--- MCMain.cpp
+++ MCMain.cpp
@@ -6,11 +6,12 @@ int main()
 {
 	double T = 1;
 	double K = 100;
-	double S_O = 100;
+	double S_O = 120;
 	double vol = 0.2;
 	double r = 0.05;
 	unsigned long N = 100000;
 
+	/*
 	PayOffCall call(K);
 	PayOffPut put(K);
 
@@ -18,5 +19,22 @@ int main()
 	double resultput = MonteCarlo(put, T, S_O, vol, r, N);
 	cout << resultcall << endl;
 	cout << resultput << endl;
+	*/
+	unsigned long optiontype;
+	cout << "Enter 0 for call and 1 for put ";
+	cin >> optiontype;
+
+	PayOff *thePayOffPtr;
+	if (optiontype==0)
+		thePayOffPtr = new PayOffCall(K);
+	else
+		thePayOffPtr = new PayOffPut(K);
+
+	double result = MonteCarlo(*thePayOffPtr,T,S_O,vol,r,N);
+	cout << result << endl;
+
+	double tmp;
+	cin >> tmp;
+	delete thePayOffPtr;
 	return 0;
 }
